6cd70564eace4d28001dd81fc499a838fc8a9806
nilo <buggyhuggymail@gmail.com>
2026-04-27 18:49:02 +0100

    adição de packages e lógica de template
    adição do package template e dos ficheiros dentro dele e do package output.
    utilização do handlebars para substituir dados no template. teste com dados rudimentares (app.ts)

--- Code/src/app/app.ts
+++ Code/src/app/app.ts
@@ -1,1 +1,33 @@
+/// <reference types="node" />
 //app to be launched through this file
+import Handlebars from "handlebars";
+import fs from 'fs';
+
+export function renderTemplate(template: any, data: { nome: string; numero: string; curso: string; data_inicio: string; data_fim: string; }) {
+    const templateSource = String(template);
+    const compiledTemplate = Handlebars.compile(templateSource);
+    const renderedHTML = compiledTemplate(data);
+    return renderedHTML;
+}
+const template = fs.readFileSync("src\\app\\template\\templatev0.html", "utf8");
+
+const renderedHTML = renderTemplate(template, {
+    nome: "Nilo Duarte",
+    numero: "48155",
+    curso: "LEIM",
+    data_inicio: "12-09-2023",
+    data_fim: "17-07-2026"
+});
+
+const list = [
+    { nome: "Nilo Duarte", numero: "48155", curso: "LEIM", data_inicio: "12-09-2023", data_fim: "17-07-2026" },
+    { nome: "Maria Silva", numero: "48156", curso: "LEIM", data_inicio: "12-09-2023", data_fim: "17-07-2026" },
+    { nome: "João Pereira", numero: "48157", curso: "LEIM", data_inicio: "12-09-2023", data_fim: "17-07-2026" }
+];
+
+for (const item of list) {
+    const renderedHTML = renderTemplate(template, item);
+    fs.writeFileSync(`src\\app\\output\\certificado_${item.numero}.html`, renderedHTML, "utf8");
+}
+
+//fs.writeFileSync("certificado.html", renderedHTML, "utf8");
