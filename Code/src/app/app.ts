427098b646c06c66da0cc13e4389061235326984
nilo <buggyhuggymail@gmail.com>
2026-05-10 12:51:46 +0100

    pupeeter integrado
    a este ponto ele já converte e guarda em PDF, o próximo passo para acabar o caso de utilização atual é apenas assinar os PDFs

--- Code/src/app/app.ts
+++ Code/src/app/app.ts
@@ -2,6 +2,7 @@
 //app to be launched through this file
 import Handlebars from "handlebars";
 import fs from 'fs';
+import puppeteer from 'puppeteer';
 
 export function renderTemplate(template: any, data: { nome: string; numero: string; curso: string; data_inicio: string; data_fim: string; }) {
     const templateSource = String(template);
@@ -31,3 +32,45 @@ for (const item of list) {
 }
 
 //fs.writeFileSync("certificado.html", renderedHTML, "utf8");
+
+async function generatePDFs() {
+
+    const files = fs.readdirSync("src/app/output");
+
+    for (const file of files) {
+
+        if (!file.endsWith(".html")) continue;
+
+        const browser = await puppeteer.launch({
+            headless: true
+        });
+
+        const page = await browser.newPage();
+
+        page.on("console", msg => console.log(msg.text()));
+        page.on("pageerror", err => console.log(err));
+
+        await page.goto(
+            `http://localhost:5500/src/app/output/${file}`,
+            {
+                waitUntil: "networkidle0"
+            }
+        );
+
+        await page.pdf({
+            path: `src/app/output/${file.replace(".html", ".pdf")}`,
+            format: "A4",
+            printBackground: true,
+            preferCSSPageSize: true
+        });
+
+        await browser.close();
+
+        console.log(`${file} convertido para PDF`);
+    }
+}
+
+generatePDFs();
+
+
+
