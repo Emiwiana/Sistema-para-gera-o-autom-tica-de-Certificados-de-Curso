30ca2f108db12c743e3f08d5a326cad8a72a7a41
nilo <buggyhuggymail@gmail.com>
2026-06-17 10:12:51 +0100

    Revert "Extract template service; update controllers/routes"
    This reverts commit 2af5acc1f832ecf9341edfc4003f684d7bcb3683.

--- Code/src/routes/templates.ts
+++ Code/src/routes/templates.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getTemplatePage, getCreateTemplatePage, getEditTemplatePage, templateCreate, templateUpdate, templateDelete, previewTemplate } from '../controllers/templates';
+import { getTemplatePage, getCreateTemplatePage, getEditTemplatePage, createTemplate, updateTemplate, deleteTemplate, previewTemplate } from '../controllers/templates';
 import { authenticate } from '../middlewares/authenticate';
 import { authorize } from '../middlewares/authorize';
 
@@ -9,7 +9,7 @@ router.get('/', authenticate, authorize, getTemplatePage);
 router.get('/create', authenticate, authorize, getCreateTemplatePage);
 router.get('/edit/:id', authenticate, authorize, getEditTemplatePage);
 
-router.post('/create', authenticate, authorize, templateCreate);
-router.post('/update', authenticate, authorize, templateUpdate);
-router.post('/delete', authenticate, authorize, templateDelete);
+router.post('/create', authenticate, authorize, createTemplate);
+router.post('/update', authenticate, authorize, updateTemplate);
+router.post('/delete', authenticate, authorize, deleteTemplate);
 router.post('/preview', authenticate, authorize, previewTemplate);
